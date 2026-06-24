2db940c535e208342a7eeee3979194345a7134ba
Muskan <muskansoni9506@gmail.com>
2026-06-24 14:43:02 +0530

    Initial DSA setup

---
+++ prac.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 27;
+    int count = 1;
+
+    while (count<=n)
+{
+    cout <<"Count is :"<<count<<endl;
+    count++;
+}   
+return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Muskan!" << endl;
+    return 0;
+}
