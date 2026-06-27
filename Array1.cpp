982bfd1fb23cb044c567eb47524d5ba7d3ec9540
Muskan <muskansoni9506@gmail.com>
2026-06-27 19:37:51 +0530

    Array

--- Array1.cpp
+++ Array1.cpp
@@ -5,11 +5,11 @@ int main(){
     int nums[]={96,77,83,02,736,376,-36,2573};
     int size =8;
 
-    int smallest = INT_MAX;
-    
+    int largest = INT_MIN;
+
     for(int i=0; i<size; i++){
-        smallest = min(smallest, nums[i]);
+        largest = max(largest, nums[i]);
     }
-    cout << "Smallest element: " << smallest << endl;
+    cout << "Largest element: " << largest << endl;
     return 0;
 }
