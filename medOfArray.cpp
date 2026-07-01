ad291c947fc39d7c0eeefb94680e72391e264648
Muskan <muskansoni9506@gmail.com>
2026-07-01 16:08:16 +0530

    Median of sorted Array

---
+++ medOfArray.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    if(nums1.size() > nums2.size())
+        return findMedianSortedArrays(nums2, nums1);
+
+    int m = nums1.size();
+    int n = nums2.size();
+
+    int low = 0;
+    int high = m;
+
+    while(low <= high){
+
+        int partition1 = (low + high) / 2;
+        int partition2 = (m + n + 1) / 2 - partition1;
+
+        int left1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+        int right1 = (partition1 == m) ? INT_MAX : nums1[partition1];
+
+        int left2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+        int right2 = (partition2 == n) ? INT_MAX : nums2[partition2];
+
+        if(left1 <= right2 && left2 <= right1){
+
+            if((m + n) % 2 == 0){
+                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            }
+            else{
+                return max(left1, left2);
+            }
+        }
+
+        else if(left1 > right2){
+            high = partition1 - 1;
+        }
+
+        else{
+            low = partition1 + 1;
+        }
+    }
+
+    return 0.0;
+}
+
+int main()
+{
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    cout << findMedianSortedArrays(nums1, nums2);
+
+    return 0;
+}
