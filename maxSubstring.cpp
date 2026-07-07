40c175595fbe2361a046b22fa69dc495b6cd4598
Muskan <muskansoni9506@gmail.com>
2026-07-07 22:41:48 +0530

    Longest Substring

---
+++ maxSubstring.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+
+    unordered_map<char, int> mp;
+
+    int left = 0;
+    int maxLen = 0;
+
+    for (int right = 0; right < s.size(); right++) {
+
+        if (mp.find(s[right]) != mp.end() && mp[s[right]] >= left) {
+            left = mp[s[right]] + 1;
+        }
+
+        mp[s[right]] = right;
+
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
+
+int main() {
+
+    string s = "abcdaaaabaccbbbbb";
+
+    cout << lengthOfLongestSubstring(s);
+
+    return 0;
+}
